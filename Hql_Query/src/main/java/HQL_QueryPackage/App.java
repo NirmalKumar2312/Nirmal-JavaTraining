2619727e13bb36e98d1d1fca0c115417d10f2d28
Nirmal Kumar <nirmalkumar23122200@gmail.com>
2022-03-31 12:01:28 +0530

    HQL-query

---
+++ Hql_Query/src/main/java/HQL_QueryPackage/App.java
@@ -0,0 +1,10 @@
+package HQL_QueryPackage;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hql_Query/src/main/java/HQL_QueryPackage/Details.java
@@ -0,0 +1,5 @@
+package HQL_QueryPackage;
+
+public class Details {
+
+}
