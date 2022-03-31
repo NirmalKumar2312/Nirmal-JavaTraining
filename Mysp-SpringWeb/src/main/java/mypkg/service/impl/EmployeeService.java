7b47ff3a7a499047ce5de71bb0b9dd0a608d29b7
Nirmal Kumar <nirmalkumar23122200@gmail.com>
2022-03-31 11:25:50 +0530

    SpringWeb

---
+++ Mysp-SpringWeb/src/main/java/mypkg/service/impl/EmployeeService.java
@@ -0,0 +1,13 @@
+package mypkg.service.impl;
+
+import java.util.Set;
+
+import mypkg.model.Employee;
+
+public interface EmployeeService {
+	
+void add(Employee employee);
+	
+	Set<Employee> employee();
+
+}
